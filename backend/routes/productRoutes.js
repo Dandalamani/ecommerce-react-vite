e6fb6dd8b62c3f639369cdbcff6cf6f7b0e9487c
Dandalamani <dandalamane@gmail.com>
2026-02-15 21:42:06 +0530

    added backend

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+router.get("/", async (req, res) => {
+  try {
+    const search = req.query.search || "";
+
+    let products = [];
+
+    // 🔹 1. Try FakeStore first
+    const fakeRes = await axios.get("https://fakestoreapi.com/products");
+
+    let filtered = fakeRes.data.filter((product) =>
+      product.title.toLowerCase().includes(search.toLowerCase())
+    );
+
+    // 🔥 2. If no results → use DummyJSON
+    if (filtered.length === 0 && search) {
+      const dummyRes = await axios.get(
+        `https://dummyjson.com/products/search?q=${search}`
+      );
+
+      // normalize data (IMPORTANT)
+      products = dummyRes.data.products.map((item) => ({
+        id: item.id,
+        title: item.title,
+        price: item.price,
+        description: item.description,
+        category: item.category,
+        image: item.thumbnail,
+      }));
+    } else {
+      products = filtered;
+    }
+
+    res.json(products);
+
+  } catch (err) {
+    console.error("🔥 ERROR:", err.message);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.log("❌ DB Error:", err));
+
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", require("./routes/productRoutes"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
